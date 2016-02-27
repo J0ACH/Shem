7dd000941b65826e33b8a9cd7fbd440ce9005589
k0f <krystof.pesek@gmail.com>
2016-02-27 18:21:54 +0100

    FindSC module fix

--- Modules/FindSC.cmake
+++ Modules/FindSC.cmake
@@ -2,26 +2,17 @@ MESSAGE(STATUS "\nFindSC init...")
 MESSAGE(STATUS "SC_SRC_DIR : " ${SC_SRC_DIR})
 
 FIND_PATH(
-    TEST_INCLUDE_DIR
-    NAMES ScBridge.h
-    #HINTS ${CMAKE_SOURCE_DIR}
-    PATHS ${CMAKE_SOURCE_DIR}/ScBridge
-    
-)
-MESSAGE(STATUS "TEST_INCLUDE_DIR : " ${TEST_INCLUDE_DIR})
-
-FIND_PATH(
     LANG_INCLUDE_DIR
-    NAMES SC_LanguageClient.hpp
-    PATHS ${SC_SRC_DIR}/lang/LangSource
+    NAMES SC_LanguageClient.hpp SC_LanguageClient.h
+    PATHS ${SC_SRC_DIR}/include/lang/
 )
 MESSAGE(STATUS "LANG_INCLUDE_DIR : " ${LANG_INCLUDE_DIR})
 
 FIND_LIBRARY(
     LANG_LINK_DIR
     NAMES sclang
-    HINTS ${SC_BUILD_DIR}/lang/Release/
-    PATHS ${SC_BUILD_DIR}/lang/Release/
+    HINTS ${SC_BUILD_DIR}/lang/Release/ ${SC_BUILD_DIR}/lang
+    PATHS ${SC_BUILD_DIR}/lang/Release/ ${SC_BUILD_DIR}/lang
 )
 
 MESSAGE(STATUS "LANG_LINK_DIR : " ${LANG_LINK_DIR})
