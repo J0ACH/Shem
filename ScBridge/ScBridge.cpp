92d0c9b3ec214f3969f9f7a3a3ffc464e8feee74
joach <jachym.pesek@gmail.com>
2016-03-08 12:22:43 +0100

    lang start

--- ScBridge/ScBridge.cpp
+++ ScBridge/ScBridge.cpp
@@ -1,21 +1,64 @@
 #include "ScBridge.h"
-//#include "SC_LanguageClient.h"
+
+#include <QtWidgets/QApplication>
+#include <QWidget>
 
 int main(int argc, char** argv){
 
-	return 0;
+	QApplication app(argc, argv);
+
+	QWidget *win = new QWidget();
+	win->setGeometry(100, 100, 300, 200);
+	win->show();
+
+	ScBridge *bridge = new ScBridge(win);	
+
+	return app.exec();
 }
 
 
-ScBridge::ScBridge()
+ScBridge::ScBridge(QObject * parent):
+QProcess(parent),
+mIpcServer(new QLocalServer(this)),
+mIpcSocket(NULL),
+mIpcServerName("SCIde_" + QString::number(QCoreApplication::applicationPid())),
+mTerminationRequested(false),
+mCompiled(false)
 {
-	/*
-    SC_LanguageClient *client = createLanguageClient("sclang");
+	this->startLang();
 
-	client->runMain();
-	client->setCmdLine("s.boot;");
-	*/
+}
 
+void ScBridge::startLang()
+{
+	QString sclangCommand = "sclang";
+
+	QString configFile;
+
+
+	QStringList sclangArguments;
+	if (!configFile.isEmpty())
+		sclangArguments << "-l" << configFile;
+	sclangArguments << "-i" << "scqt";
+
+	QProcess::start(sclangCommand, sclangArguments);
+	bool processStarted = QProcess::waitForStarted();
+	//if (!processStarted)
+		//emit statusMessage(tr("Failed to start interpreter!"));
+
+}
+
+void ScBridge::read()
+{
+	if (mTerminationRequested) {
+		// when stopping the language, we don't want to post for longer than 200 ms to prevent the UI to freeze
+		if (QDateTime::currentDateTimeUtc().toMSecsSinceEpoch() - mTerminationRequestTime.toMSecsSinceEpoch() > 200)
+			return;
+	}
+
+	QByteArray out = QProcess::readAll();
+	QString postString = QString::fromUtf8(out);
+	//emit scPost(postString);
 }
 
 ScBridge::~ScBridge()
--- ScBridge/ScBridge.h
+++ ScBridge/ScBridge.h
@@ -1,17 +1,41 @@
 #ifndef SCBRIDGE_H
 #define SCBRIDGE_H
 
+#include <QAction>
+#include <QByteArray>
+#include <QDateTime>
+#include <QDebug>
+#include <QProcess>
+#include <QThread>
+#include <QUuid>
+#include <QtNetwork/QLocalSocket>
+#include <QtNetwork/QLocalServer>
 
-class ScBridge
+class ScBridge : public QProcess
 {
-public:
-	ScBridge();
-	~ScBridge();
-private:
+	Q_OBJECT
 
+public:
+	ScBridge(QObject *parent);
+	~ScBridge();
+
+	void startLang();
+	void read();
+
+private:
+	QLocalServer *mIpcServer;
+	QLocalSocket *mIpcSocket;
+	QString mIpcServerName;
+
+	bool mTerminationRequested;
+	QDateTime mTerminationRequestTime;
+	bool mCompiled;
 };
 
 
 
 
 #endif // SCBRIDGE
+
+
+	
