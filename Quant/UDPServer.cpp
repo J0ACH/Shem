f5caeecc53699e2d277479ad3eddca0aa2371557
k0f <krystof.pesek@gmail.com>
2016-05-06 13:13:06 +0200

    username resolution

--- Quant/UDPServer.cpp
+++ Quant/UDPServer.cpp
@@ -5,160 +5,169 @@
 
 namespace QuantIDE {
 
-	UDPServer::UDPServer(QWidget *parent) : QWidget(parent)
-	{
-		setObjectName("UDPServer");
-		objectPattern = QString::null;
-		connect(parent, SIGNAL(actConfigDone()), this, SLOT(initSocket()));
-		connect(this, SIGNAL(actPrintStatus(QString)), parent, SLOT(onMsgStatus(QString)));
-		connect(this, SIGNAL(actPrintMsg(QString)), parent, SLOT(onMsgNormal(QString)));
-		/*
-		if(this->initSocket()==0){
-		//print to graphical console?
-		//this->println("UDP Server booted");
-		}
-		*/
-	}
-	/*
-	void println(const char *input){
-	emit println(input);
-	}
-	*/
+UDPServer::UDPServer(QWidget *parent) : QWidget(parent) {
+  setObjectName("UDPServer");
+  objectPattern = QString::null;
 
-	bool UDPServer::isConnectedToNet(){
-		QList<QNetworkInterface> ifaces = QNetworkInterface::allInterfaces();
-		bool result = false;
+  connect(parent, SIGNAL(actConfigDone()), this, SLOT(initSocket()));
+  connect(this, SIGNAL(actPrintStatus(QString)), parent,
+          SLOT(onMsgStatus(QString)));
+  connect(this, SIGNAL(actPrintMsg(QString)), parent,
+          SLOT(onMsgNormal(QString)));
+}
 
-		for (int i = 0; i < ifaces.count(); i++){
-			QNetworkInterface iface = ifaces.at(i);
-			if (iface.flags().testFlag(QNetworkInterface::IsUp)
-				&& !iface.flags().testFlag(QNetworkInterface::IsLoopBack)){
+bool UDPServer::isConnectedToNet() {
+  QList<QNetworkInterface> ifaces = QNetworkInterface::allInterfaces();
+  bool result = false;
+
+  for (int i = 0; i < ifaces.count(); i++) {
+    QNetworkInterface iface = ifaces.at(i);
+    if (iface.flags().testFlag(QNetworkInterface::IsUp) &&
+        !iface.flags().testFlag(QNetworkInterface::IsLoopBack)) {
 
 #ifdef DEBUG
-				qDebug() << "UDP: got interface:" << iface.name() << "with mac:" << iface.hardwareAddress();
+      qDebug() << "UDP: got interface:" << iface.name()
+               << "with mac:" << iface.hardwareAddress();
 #endif
 
-				for (int j = 0; j < iface.addressEntries().count(); j++){
+      for (int j = 0; j < iface.addressEntries().count(); j++) {
 #ifdef DEBUG
-					QString _broadcast = iface.addressEntries().at(j).broadcast().toString();
-					qDebug() << "UDP: addr.:" << iface.addressEntries().at(j).ip().toString();
-					qDebug() << "UDP: netmask:" << iface.addressEntries().at(j).netmask().toString();
-					qDebug() << "UDP: broadcast:" << _broadcast;
+        QString _broadcast =
+            iface.addressEntries().at(j).broadcast().toString();
+        qDebug() << "UDP: addr.:"
+                 << iface.addressEntries().at(j).ip().toString();
+        qDebug() << "UDP: netmask:"
+                 << iface.addressEntries().at(j).netmask().toString();
+        qDebug() << "UDP: broadcast:" << _broadcast;
 #endif
 
-					if (result == false){
-						result = true;
-						interface = new QNetworkInterface(iface);
-					}
+        if (result == false) {
+          result = true;
+          interface = new QNetworkInterface(iface);
+        }
 
-					if (_broadcast.size() > 0){
-						hasBroadcast = true;
-						addressSelector = j;
-					}
-				}
+        if (_broadcast.size() > 0) {
+          hasBroadcast = true;
+          addressSelector = j;
+        }
+      }
 
 #ifdef DEBUG
-				if (!result)
-					qDebug() << "UDP: interface seems to have no address";
+      if (!result)
+        qDebug() << "UDP: interface seems to have no address";
 #endif
-			}
+    }
+  }
 
-		}
+  return result;
+}
 
-		return result;
-	}
+QString UDPServer::getUsername() {
 
-	int UDPServer::initSocket(){
-		port = PORT;
+  QString name = qgetenv("USER");
+  if (name.isEmpty())
+    name = qgetenv("USERNAME");
+  // qDebug() << "UDP: username: " << name;
+  return name;
+}
 
-		emit actPrintStatus("UDP Server init...");
-		emit actPrintMsg("UDP: Checking network setup...");
+int UDPServer::initSocket() {
+  port = PORT;
+  username = new QString(this->getUsername());
 
-		if (isConnectedToNet()) { emit actPrintMsg("UDP: Network link is ESTABLISHED"); }
-		else {
-			emit actPrintMsg("UDP: ERROR! you are not connected to any network.");
-			return 1;
-		}
+  emit actPrintStatus("UDP Server init...");
+  emit actPrintMsg(tr("UDP: my username: %1").arg(*username));
 
-		int count = 0;
-		foreach(const QHostAddress &address, QNetworkInterface::allAddresses()) {
-			if (address.protocol() == QAbstractSocket::IPv4Protocol && address != QHostAddress(QHostAddress::LocalHost)){
-				if (count == 0) { 
-					emit actPrintMsg(tr("UDP: local IP address: %1").arg(address.toString()));
-				}
-				else {
-					emit actPrintMsg("UDP WARN: multiple network addresses detected, picking first one");
-				}
-			}
-		}
+  emit actPrintMsg("UDP: Checking network setup...");
 
-		if (hasBroadcast){
-			QString _baddress = interface->addressEntries().at(addressSelector).broadcast().toString();
-			broadcastAddress = new QHostAddress(_baddress);
-			emit actPrintMsg(tr("UDP: OK broadcast addr.: %1").arg(_baddress));
-		}
-		else{
-			emit actPrintMsg("UDP: WARNING: network seems to have no Broadcast support, setting default one.");
-			broadcastAddress = new QHostAddress("239.0.0.1");
-		}
+  if (isConnectedToNet()) {
+    emit actPrintMsg("UDP: Network link is ESTABLISHED");
+  } else {
+    emit actPrintMsg("UDP: ERROR! you are not connected to any network.");
+    return 1;
+  }
 
-		emit actPrintMsg(tr("UDP: Server starting, listening at port: %1").arg(QString::number(port)));
+  int count = 0;
+  foreach (const QHostAddress &address, QNetworkInterface::allAddresses()) {
+    if (address.protocol() == QAbstractSocket::IPv4Protocol &&
+        address != QHostAddress(QHostAddress::LocalHost)) {
+      if (count == 0) {
+        emit actPrintMsg(
+            tr("UDP: local IP address: %1").arg(address.toString()));
+      } else {
+        emit actPrintMsg(
+            "UDP WARN: multiple network addresses detected, picking first one");
+      }
+    }
+  }
 
-		socket = new QUdpSocket(this);
-		host = new QHostInfo();
-		socket->bind(QHostAddress::Any, port);
+  if (hasBroadcast) {
+    QString _baddress =
+        interface->addressEntries().at(addressSelector).broadcast().toString();
+    broadcastAddress = new QHostAddress(_baddress);
+    emit actPrintMsg(tr("UDP: OK broadcast addr.: %1").arg(_baddress));
+  } else {
+    emit actPrintMsg("UDP: WARNING: network seems to have no Broadcast "
+                     "support, setting default one.");
+    broadcastAddress = new QHostAddress("239.0.0.1");
+  }
 
-		if (socket->state() == 4){
-			emit actPrintStatus("UDP: Server is ON!");
-		}
-		else{
-			emit actPrintMsg(tr("UDP: There is a problem starting the server on port %1").arg(QString::number(port)));
-			return 1;
-		}
+  emit actPrintMsg(tr("UDP: Server starting, listening at port: %1")
+                       .arg(QString::number(port)));
 
-		emit actPrintMsg("///////////////////////////////////////////////////////////////////");
+  socket = new QUdpSocket(this);
+  host = new QHostInfo();
+  socket->bind(QHostAddress::Any, port);
 
+  if (socket->state() == 4) {
+    emit actPrintStatus("UDP: Server is ON!");
+  } else {
+    emit actPrintMsg(
+        tr("UDP: There is a problem starting the server on port %1")
+            .arg(QString::number(port)));
+    return 1;
+  }
 
-		connect(socket, SIGNAL(readyRead()), this, SLOT(readPendingDatagrams()));
+  emit actPrintMsg(
+      "///////////////////////////////////////////////////////////////////");
 
-		this->send("Hello there! -- test message");
-		return 0;
-	}
+  connect(socket, SIGNAL(readyRead()), this, SLOT(readPendingDatagrams()));
 
-	void UDPServer::send(const char *input){
+  this->send((tr("Hi all my name is %1").arg(*username)).toStdString().c_str());
+  return 0;
+}
 
-		QByteArray datagram(input);
-		socket->writeDatagram(datagram.data(), datagram.size(),
-			*broadcastAddress, port);
-	}
+void UDPServer::send(const char *input) {
 
-	void UDPServer::readPendingDatagrams(){
+  QByteArray datagram(input);
+  socket->writeDatagram(datagram.data(), datagram.size(), *broadcastAddress,
+                        port);
+}
 
-		while (socket->hasPendingDatagrams()) {
+void UDPServer::readPendingDatagrams() {
 
-			QByteArray datagram;
-			datagram.resize(socket->pendingDatagramSize());
+  while (socket->hasPendingDatagrams()) {
 
-			QHostAddress sender;
-			quint16 senderPort;
+    QByteArray datagram;
+    datagram.resize(socket->pendingDatagramSize());
 
-			socket->readDatagram(datagram.data(), datagram.size(), &sender,
-				&senderPort);
+    QHostAddress sender;
+    quint16 senderPort;
 
+    socket->readDatagram(datagram.data(), datagram.size(), &sender,
+                         &senderPort);
 
-			emit actPrintMsg("UDP: Got some data chef!");
-			emit actPrintMsg(tr("UDP: sender: %1").arg(sender.toString()));
-			emit actPrintMsg(tr("UDP: data: %1").arg(datagram.data()));
+    emit actPrintMsg("UDP: Got some data chef!");
+    emit actPrintMsg(tr("UDP: sender: %1").arg(sender.toString()));
+    emit actPrintMsg(tr("UDP: data: %1").arg(datagram.data()));
 
+    processDatagram(datagram);
+  } // end while
+} // end void
 
-			processDatagram(datagram);
-		} // end while
-	}   // end void
+void UDPServer::processDatagram(QByteArray datagram) {}
 
-	void UDPServer::processDatagram(QByteArray datagram){
-	}
-
-	UDPServer::~UDPServer() {};
-	// end class
+UDPServer::~UDPServer(){};
+// end class
 
 } // end namespace
--- Quant/UDPServer.h
+++ Quant/UDPServer.h
@@ -12,52 +12,50 @@
 #include <QTextEdit>
 #include <QWidget>
 
-#include <QtNetwork/QUdpSocket>
 #include <QHostInfo>
 #include <QNetworkInterface>
+#include <QtNetwork/QUdpSocket>
 
 using namespace Jui;
 using namespace SupercolliderBridge;
 
 namespace QuantIDE {
 
-  class UDPServer : public QWidget
-  {
-    Q_OBJECT
+class UDPServer : public QWidget {
+  Q_OBJECT
 
-    public:
-      UDPServer(QWidget *parent = 0);
-      ~UDPServer();
+public:
+  UDPServer(QWidget *parent = 0);
+  ~UDPServer();
 
-      public slots:
-	   int initSocket();
-        void readPendingDatagrams();
-      void send(const char *input);
+public slots:
+  int initSocket();
+  void readPendingDatagrams();
+  void send(const char *input);
 
+signals:
+  // void println(const char *input);
+  void actPrintStatus(QString);
+  void actPrintMsg(QString);
 
-signals: 
-     // void println(const char *input);
-	  void actPrintStatus(QString);
-	  void actPrintMsg(QString);
+private:
+  //     int initSocket();
+  void pendingDatagramSize();
+  bool isConnectedToNet();
+  int port;
+  int addressSelector;
+  bool hasBroadcast;
+  void processDatagram(QByteArray);
+  QString getUsername();
 
-
-    private:
- //     int initSocket();
-      void pendingDatagramSize();
-      bool isConnectedToNet();
-      int port;
-      int addressSelector;
-      bool hasBroadcast;
-      void processDatagram(QByteArray);
-
-
-      QNetworkInterface *interface;
-      QHostAddress *broadcastAddress;
-      QHostAddress *myAddress;
-      QUdpSocket *socket;
-      QHostInfo *host;
-      QString objectPattern;
-  };
+  QNetworkInterface *interface;
+  QHostAddress *broadcastAddress;
+  QHostAddress *myAddress;
+  QUdpSocket *socket;
+  QHostInfo *host;
+  QString *username;
+  QString objectPattern;
+};
 }
 
 #endif // end UDPServer
